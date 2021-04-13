ea1cd0f3ffc9a785321414cb4f899be8b853ac70
alextyang <koopakondra@gmail.com>
2021-04-12 20:54:06 -0400

    Initial

---
+++ script.js
@@ -0,0 +1,165 @@
+
+var jsonData = {
+  "works" : [{
+      "name":"Mattress Performance (Carry That Weight)",
+      "artist":"Emma Sulkowicz",
+      "year":"2014 - 2015",
+      "image":"https://news.artnet.com/app/news-upload/2016/07/47432056.cached.jpg",
+      "desc":"Still making headlines today, Emma Sulkowicz’s enduring performance (begun in September 2014 as a thesis project) involved carrying a 50-pound mattress around with her wherever she went on New York City’s Columbia campus, where she was alleges she was raped by a fellow student in 2012. The artist promised the piece would end only if and when her supposed rapist was expelled from the university. He was not, so Sulkowicz carried the mattress onstage during her graduation ceremony to bring the performance to a close in May 2015—not before making a lasting impression on critics and the public alike."
+    }, {
+      "name":"ART WHORE",
+      "artist":"Ryder Ripps",
+      "year":"2014",
+      "image":"https://news.artnet.com/app/news-upload/2016/07/consent-is-sexy-in-defense-of-ryder-ripps-controversial-artwhore-body-image-1415992274.jpg",
+      "desc":"After accepting the offer of a one-night artist’s residency at New York’s Ace Hotel, Ryder Ripps presented “ART WHORE,” a project in which he invited two escorts acquired through craigslist to join him in his hotel room. During their stay, Ripps documented Brooke and Jay after he instructed them to draw whatever they wished. Art F City writer Whitney Kimball described “ART WHORE” as being “in the running for the most offensive project of 2014.” Dazed’s Zing Tsjeng described it as “the same old story: a white dude co-opting someone else’s labor in his struggle to Make A Point. Nothing new about that.”"
+    }, {
+      "name":"Eres Lo Que Lees (You Are What You Read)",
+      "artist":"Guillermo Vargas",
+      "year":"2007",
+      "image":"https://news.artnet.com/app/news-upload/2016/07/dd111.jpg",
+      "desc":"Guillermo Vargas’ story is in a particularly sensitive vein, as it involves the supposedly-intentional starvation of an innocent animal. In an exhibition at Códice Gallery in Managua, Nicaragua, the artist bound a stray dog to the wall by a leash, leaving it without food or water. Spelled out above the animal were the words, “Eres Lo Que Lees,” Spanish for “You Are What You Read.” To add insult to injury, the phrase was made out of dog biscuits.\n\nThe gallery defended itself, claiming the dog was unchained and fed outside of exhibition hours, but that didn’t prevent international outrage or the death threats addressed to Vargas from streaming in."
+    }, {
+      "name":"The Holy Virgin Mary",
+      "artist":"Chris Ofili",
+      "year":"1996",
+      "image":"https://osmanerden.files.wordpress.com/2013/01/37.jpg",
+      "desc":"Though created in 1996, this now-infamous painting by Turner Prize winner Chris Ofili didn’t cause a stir until 1999, when it was scheduled to be shown in “Sensation,” a traveling group exhibition of YBA works bound for New York City’s Brooklyn Museum. The large painting depicts a black Virgin Mary and incorporates elephant dung as well as collaged pornographic images. Then-Mayor Rudy Giuliani, a devout Catholic, called the work “sick” and attempted to block the exhibition by cutting the museum’s funding. His request was denied by a federal judge, and the painting eventually went on to sell for $4.6 million at auction."
+    }, {
+      "name":"Everyone I Have Ever Slept With 1963-1995",
+      "artist":"Tracey Emin",
+      "year":"1995",
+      "image":"https://news.artnet.com/app/news-upload/2016/07/emin-everyone-i-have-ever-slept-with.jpg",
+      "desc":"One can guess what might be controversial here based on the name of Tracey Emin’s work alone, but the title is actually misleading. Emin created a tent on which she appliquéd the 102 names of everyone she had slept with or beside, in either a sexual or platonic sense. Of the work, Emin said, “Some I’d had a shag with in bed or against a wall. Some I had just slept with, like my grandma. I used to lay in her bed and hold her hand. We used to listen to the radio together and nod off to sleep. You don’t do that with someone you don’t love and don’t care about.” The work was later destroyed in an East London warehouse fire, along with over 100 other works owned by Charles Saatchi."
+    }, {
+      "name":"Do Women Have to be Naked to get into the Met. Museum?",
+      "artist":"Guerrilla Girls",
+      "year":"1989",
+      "image":"https://www.artnews.com/wp-content/uploads/2016/01/guerrilla-girls.jpg",
+      "desc":"Ever the art-world radicals, the Guerrilla Girls are an anonymous group of female artists that was formed in response to a 1984 painting and sculpture survey at New York’s Museum of Modern Art that included just 13 women out of a total of 169 artists. Thus began a poster campaign to raise awareness of the lack of women artists represented in major contemporary art establishments.\n\nThough they pitched the above poster to the Public Art Fund for a New York billboard, the work was rejected, and in retaliation, the Guerrilla Girls rented space on New York City buses for the ad."
+    }, {
+      "name":"X Portfolio",
+      "artist":"Robert Mapplethorpe",
+      "year":"1989",
+      "image":"https://news.artnet.com/app/news-upload/2016/07/mapplethorpe-corcoran.jpg",
+      "desc":"In June 1989—just a few months after his untimely death from AIDS—a retrospective of over 150 of Robert Mapplethorpe’s works was due to open at the Corcoran Art Gallery in Washington, DC. However, the sexually explicit nature of the photographs, which featured sado-masochism and homosexuality, drew the wrath of Capitol Hill’s conservative lawmakers. In a misguided attempt to avoid controversy, the exhibition was canceled, adding fuel to the so-called “Culture Wars.” In protest, Mapplethorpe supporters congregated outside the gallery on the evening of June 30, 1989, projecting giant images of his work onto the side of the building, creating a powerful and moving tribute to the late photographer."
+    }, {
+      "name":"Piss Christ",
+      "artist":"Andres Serrano",
+      "year":"1987",
+      "image":"https://news.artnet.com/app/news-upload/2016/07/Serrano-Piss-Christ.jpg",
+      "desc":"For more than two decades, Andres Serrano’s Piss Christ has been a polarizing work for its subject matter. The photograph of a plastic crucifix submerged in a cup of the artist’s urine has sparked controversy since its creation, but public anger came to a climax in 2011 in Avignon, France, when Catholic fundamentalists attempted to destroy the photograph with hammers.\n\n“At the time I made Piss Christ, I wasn’t trying to get anything across,” Serrano told the Guardian in 2012. “In hindsight, I’d say Piss Christ is a reflection of my work, not only as an artist, but as a Christian.”"
+    }, {
+      "name":"Tilted Arc",
+      "artist":"Richard Serra",
+      "year":"1981",
+      "image":"https://news.artnet.com/app/news-upload/2016/07/GettyImages-3229706-1024x672.jpg",
+      "desc":"Richard Serra’s 120-foot-long and 12-foot-high bend of Cor-Ten steel hardly seems like the description of a work that could induce the anger like that of medieval villagers with torches and pitchforks. But such was the case in 1981, when Serra erected his massive wall-like sculpture, bisecting New York City’s Federal Plaza.\n\nDenounced for its placement in the middle of a busy pedestrian thoroughfare, it was seen as an annoying obstruction—as a result, it was removed from the plaza in 1989, despite Serra’s intention that it would be permanent. “I don’t think it is the function of art to be pleasing,” he commented at the time. “Art is not democratic. It is not for the people.”"
+    }, {
+      "name":"Fountain",
+      "artist":"Marcel Duchamp",
+      "year":"1917",
+      "image":"https://news.artnet.com/app/news-upload/2016/07/duchamp-fountain.jpg",
+      "desc":"Perhaps the most famous controversial work of all time, Marcel Duchamp’s Fountain, arguably the first “readymade” in existence. As a porcelain urinal turned upon its back, the piece was submitted under the pseudonym “R. Mutt” to the Society of Independent Artists, and was promptly excluded from the Society’s exhibition. Duchamp resigned from the board in protest.\n\nIn a 1964 interview, Duchamp—by then the known producer of the work—was quoted in the Paris-Express as saying, “I was drawing people’s attention to the fact that art is a mirage. A mirage, exactly like an oasis appears in the desert. It is very beautiful until, of course, you are dying of thirst. But you don’t die in the field of art. The mirage is solid.”"
+    }]
+};
+
+var currentIndex = 0;
+let contentDiv = document.getElementById("content");
+
+
+window.addEventListener("DOMContentLoaded", function() {
+  jsonData.works.forEach(loadSlide);
+});
+
+function loadSlide(slide, i) {
+  let slideDiv = document.createElement("div")
+  slideDiv.className = 'slide'
+  slideDiv.id = 'slide'+i
+  contentDiv.appendChild(slideDiv)
+
+  let name = document.createElement("H1")
+  name.innerText = slide.name
+  name.className = 'name'
+  slideDiv.appendChild(name)
+
+  let artist = document.createElement("H2")
+  artist.innerText = slide.artist + "\u00a0\u00a0" + slide.year
+  artist.className = 'artist'
+  slideDiv.appendChild(artist)
+
+  let image = document.createElement("IMG")
+  image.setAttribute('src', slide.image)
+  image.className = 'image'
+  slideDiv.appendChild(image)
+
+  let desc = document.createElement("P")
+  desc.innerText = slide.desc
+  desc.className = 'desc'
+  slideDiv.appendChild(desc)
+}
+
+
+
+
+
+
+function pageLeft() {
+  let oldIndex = currentIndex;
+  currentIndex -= 1
+  if (currentIndex < 0) {
+    currentIndex = 9
+  }
+
+  let oldSlide = document.getElementById('slide'+oldIndex)
+  oldSlide.style.transform = "translateX(-50px)";
+  oldSlide.style.opacity = "0";
+
+  var nodes = contentDiv.children
+  for(var i=0; i<nodes.length; i++) {
+    if (nodes[i] != oldSlide) {
+      nodes[i].style.transform = "translateX(50px)";
+    }
+  }
+
+  setTimeout(() => {
+    oldSlide.style.pointerEvents = "none";
+    oldSlide.style.transform = "translateX(50px)";
+  }, 400);
+
+  let newSlide = document.getElementById('slide'+currentIndex)
+
+  newSlide.style.opacity = "1";
+  newSlide.style.transform = "translateX(0px)";
+  newSlide.style.pointerEvents = "all";
+}
+
+
+function pageRight() {
+  let oldIndex = currentIndex;
+  currentIndex += 1
+  if (currentIndex > 9) {
+    currentIndex = 0
+  }
+
+  let oldSlide = document.getElementById('slide'+oldIndex)
+  oldSlide.style.transform = "translateX(50px)";
+  oldSlide.style.opacity = "0";
+
+  var nodes = contentDiv.children
+  for(var i=0; i<nodes.length; i++) {
+    if (nodes[i] != oldSlide) {
+      nodes[i].style.transform = "translateX(-50px)";
+    }
+  }
+
+  setTimeout(() => {
+    oldSlide.style.pointerEvents = "none";
+    oldSlide.style.transform = "translateX(-50px)";
+  }, 400);
+
+  let newSlide = document.getElementById('slide'+currentIndex)
+
+  newSlide.style.opacity = "1";
+  newSlide.style.transform = "translateX(0px)";
+  newSlide.style.pointerEvents = "all";
+}
